ad6e1b0dbb930abed3695c76970cef5546fe99b8
AllaSmithll <allanammancio2004@gmail.com>
2022-09-07 20:54:26 -0300

    ex073.py - 07/09/2022

---
+++ 5- Tuplas, Listas e Dicionário/ex073.py
@@ -0,0 +1,10 @@
+#Campeonato Brasileiro no dia 07/09/2022
+brasileirao = ('PALMEIRAS', 'FLAMENGO', 'CORINTHIANS', 'INTERNACIONAL', 'FLUMINENSE', 'ATHLÉTICO-PR', 'ATLÉTICO-MG', 'AMÉRICA-MG', 'GOIÁS', 'SANTOS', 'BRAGANTINO', 'FORTALEZA', 'BOTAFOGO', 'SÃO PAULO', 'CEARÁ', 'CUIABÁ', 'CORITIBA', 'AVAÍ', 'ATLÉTICO-GO', 'JUVENTUDE')
+
+brasa_primeiros = 
+for i in brasileirao: #Os 6 primeiros colocados
+    if i == 0:
+        brasa_primeiros = brasileirao[i]
+    else:
+        pass
+print(brasa_primeiros)
