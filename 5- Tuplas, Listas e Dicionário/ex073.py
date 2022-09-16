248d830820fee690f2111a55d4bd6a819349efbd
AllaSmithll <allanammancio2004@gmail.com>
2022-09-16 07:48:57 -0300

    exercício-073 concluído

--- 5- Tuplas, Listas e Dicionário/ex073.py
+++ 5- Tuplas, Listas e Dicionário/ex073.py
@@ -1,12 +1,8 @@
 #Campeonato Brasileiro no dia 07/09/2022
 brasileirao = ('PALMEIRAS', 'FLAMENGO', 'CORINTHIANS', 'INTERNACIONAL', 'FLUMINENSE', 'ATHLÉTICO-PR', 'ATLÉTICO-MG', 'AMÉRICA-MG', 'GOIÁS', 'SANTOS', 'BRAGANTINO', 'FORTALEZA', 'BOTAFOGO', 'SÃO PAULO', 'CEARÁ', 'CUIABÁ', 'CORITIBA', 'AVAÍ', 'ATLÉTICO-GO', 'JUVENTUDE')
+print(f'Lista de times: {brasileirao}')
 
-brasa_primeiros = ['']
-for i in brasileirao: #Os 6 primeiros colocados
-    if i == 0:
-        brasa_primeiros.append(brasileirao[i])
-    elif i == 1 or i == 2 or i == 3 or i == 4 or i == 5:
-        brasa_primeiros.append(brasileirao[i])
-    else:
-        pass
-print(brasa_primeiros)
+print(f"\nOs 6 primeiros times são: {brasileirao[0:6]}")
+print(f"Os 4 últimos times são: {brasileirao[16:21]}")
+print(f"Times em ordem alfabética: {sorted(brasileirao)}")
+print(f"Corinthians: {brasileirao.index('CORINTHIANS')+1}ª posição.")
