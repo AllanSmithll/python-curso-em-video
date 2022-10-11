96df47916e0bb7f0361b9ec1919413c23a5dbe2c
AllaSmithll <allanammancio2004@gmail.com>
2022-10-11 07:45:09 -0300

    término ex 78 e começo do ex 79

--- 5- Tuplas, Listas e Dicionário/ex079.py
+++ 5- Tuplas, Listas e Dicionário/ex079.py
@@ -1,6 +1,14 @@
-'''numeros = list()
-while escolha == 'S':
-    escolha = input('Vai escolher algum número (s ou n)? ').upper()
-    numeros.append(float(input('Digite um número: ')))
-print(numeros)'''
+# 11/10/2022
+# Vários valores numéricos e cadastrar em uma lista. Caso o número já exista, ele não será adicionado. No final, mostrar todos os números diferentes, e em ordem crescente
+
+numeros = list()
+escolha = "S"
+while escolha == 'S':  # Continue enquanto o usuário escolher "SIM"
+    numeros.append(float(input('Digite um número: '))) # Solicite
+    escolha = input('Vai escolher algum número (s ou n)? ').upper() # Vai escolher mais um número?
+    if escolha == "S":
+        continue   # Se sim, continue solicitando
+    elif escolha == 'N': 
+        break      # Se não, pare de solicitar
+print(numeros)
 
