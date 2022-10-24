03f5e50fbcf93740a888bfef409b3020d47e9bed
AllanSmithll <allanammancio2004@gmail.com>
2022-10-23 21:51:04 -0300

    Exercício de Cadastro de números sem sort()

--- 5- Tuplas, Listas e Dicionário/ex080.py
+++ 5- Tuplas, Listas e Dicionário/ex080.py
@@ -1,4 +1,18 @@
 # 18/10/2022
 # Cadastrar números numa lista de forma ordenada sem usar sort()
 
+lista = []
 
+for i in range(0, 5):
+    num = float(input("Digite um número para a lista: "))
+    if i == 0 or num > lista[-1]:
+        lista.append(num)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if num <= lista[pos]:
+                lista.insert(pos, num)
+                break
+            pos += 1
+
+print(lista)
