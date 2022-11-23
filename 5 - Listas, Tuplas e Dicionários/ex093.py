db2619bfef0a4782cc2100439bd922eb426f8d75
AllanSmithll <allanammancio2004@gmail.com>
2022-11-22 21:57:23 -0300

    Exercício 093 iniciado

---
+++ 5 - Listas, Tuplas e Dicionários/ex093.py
@@ -0,0 +1,14 @@
+# 22/11/2022
+'''
+Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+'''
+
+jogador = dict()
+
+jogador = {
+    "nome": input("Nome: "),
+    "partidas_jogadas": input("Partidas Jogadas: "),
+}
+
+
+print(jogador)
