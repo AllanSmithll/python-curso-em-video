5cf30c39eb93e9caa5727e4587a36e0ed7871db7
AllaSmithll <allanammancio2004@gmail.com>
2022-12-08 14:01:02 -0300

    Término do exercício

--- 6 - Funções/ex097.py
+++ 6 - Funções/ex097.py
@@ -1,20 +1,20 @@
 # 30/11/2022
 '''
-Exercício Python 097: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.           
+Exercício Python 097: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
 '''
 
-# def escreva(txt):
-#     if len(txt) == 0: return None
-#     else:
-#         if len(txt) == 1:
-#             print('~~~')
-#             print(txt)
-#             print('~~~'*len(txt))
-#         elif len(txt) > 1:
-#             print('~~~~'*len(txt) / 2)
-#             print(txt)
-#             print('~~~~'*len(txt) / 2)
+def escreva(txt) -> str:
+    if len(txt) == 0: return None
+    else:
+        if len(txt) == 1:
+            print('~~~')
+            print(txt)
+            print('~~~'*len(txt))
+        elif len(txt) > 1:
+            print('~~~'*(len(txt) // 2))
+            print(txt)
+            print('~~~'*(len(txt) // 2))
 
-# # Programa principal
-# texto = input("Palavra para formatarmos bonitinho: ")
-# print(escreva(texto))
+# Programa principal
+texto = input("Palavra para formatarmos bonitinho: ")
+print(escreva(texto))
